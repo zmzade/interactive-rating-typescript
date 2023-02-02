28ad08329ef8962b9f664ed5d8ff9770c463db57
zmzade <zmahmoodzadeh@yahoo.com>
2023-02-02 20:43:35 +0100

    remove top div

--- src/components/ThankyouCard.tsx
+++ src/components/ThankyouCard.tsx
@@ -8,17 +8,15 @@ type ThankyouCardProps = {
 const ThankyouCard = ({ rating }: ThankyouCardProps) => {
   return (
     <Card>
-      <div className="thank-container">
-        <div className="thankImg-container">
-          <img src={thankyouImg} alt="thank you" />
-        </div>
-        <p className="message">You selected {rating} out of 5</p>
-        <h2 className="title">Thank you!</h2>
-        <p className="text">
-          We appreciate you taking the time to give a rating. If you ever need
-          more support, don’t hesitate to get in touch!
-        </p>
+      <div className="thankImg-container">
+        <img src={thankyouImg} alt="thank you" />
       </div>
+      <p className="message">You selected {rating} out of 5</p>
+      <h2 className="title">Thank you!</h2>
+      <p className="text">
+        We appreciate you taking the time to give a rating. If you ever need
+        more support, don’t hesitate to get in touch!
+      </p>
     </Card>
   );
 };

a43f9e2d7494f3764fca4d48ef93a27524c78ab7
zmzade <zmahmoodzadeh@yahoo.com>
2023-02-02 20:42:09 +0100

    remove Card comp from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Card from "./components/Card";
 import RatingCard from "./components/RatingCard";
 import ThankyouCard from "./components/ThankyouCard";
 
@@ -8,20 +7,18 @@ function App() {
   const [rating, setRating] = React.useState<number>(0);
 
   return (
-    <Card>
-      <>
-        {showThankCard ? (
-          <ThankyouCard rating={rating} />
-        ) : (
-          <RatingCard
-            rating={rating}
-            setRating={setRating}
-            showThankCard={showThankCard}
-            setShowThankCard={setShowThankCard}
-          />
-        )}
-      </>
-    </Card>
+    <>
+      {showThankCard ? (
+        <ThankyouCard rating={rating} />
+      ) : (
+        <RatingCard
+          rating={rating}
+          setRating={setRating}
+          showThankCard={showThankCard}
+          setShowThankCard={setShowThankCard}
+        />
+      )}
+    </>
   );
 }
 
